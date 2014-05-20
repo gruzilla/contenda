2308db35de44ef833da4210d2db8850adc6dbdfb
gruzilla <grillen@abendstille.at>
2014-05-20 13:02:56 +0200

    added custom theme

---
+++ wordpress/wp-content/themes/bones/logo-area.php
@@ -0,0 +1,15 @@
+<div class="logo-area">
+    <div class="department">
+        <p class="contenda">Contenda</p>
+        <p>Wirtschaftstreuhand Gesellschaft m.b.H</p>
+    </div>
+    <div class="department">
+        <p class="dahm">DKFM. Dr. Henriette Dahm</p>
+        <p>Wirtschaftstreuhänder und Steuerberater</p>
+    </div>
+    <div class="department last">
+        <p class="interaccount">Interaccount</p>
+        <p>Wirtschaftstreuhand Gesellschaft m.b.H</p>
+    </div>
+</div>
+<small class="byline">TEL 512 86 70 - FAX 513 32 16</small>
