98fe9ae1f8456a15fc0621a7263348b02cb0a72a
gruzilla <grillen@abendstille.at>
2014-06-03 15:37:18 +0200

    fixed minor layout problems

--- wordpress/wp-content/themes/bones/logo-area.php
+++ wordpress/wp-content/themes/bones/logo-area.php
@@ -1,4 +1,4 @@
-<div class="logo-area">
+<div class="logo-area" onclick="window.location.href='<?php echo get_site_url(); ?>'">
     <div class="department">
         <p class="contenda">Contenda</p>
         <p>Wirtschaftstreuhand Gesellschaft m.b.H</p>
@@ -12,4 +12,4 @@
         <p>Wirtschaftstreuhand Gesellschaft m.b.H</p>
     </div>
 </div>
-<small class="byline">TEL 512 86 70 - FAX 513 32 16</small>
+<small class="byline">TEL +43 (1) 512 86 70 - FAX +43 (1) 513 32 16 - wth@contenda.at</small>
